80c3579c6c3f3aec5ede0e18f7f845d33eb75c4e
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-05-09 20:16:16 +0200

    setup

---
+++ certificaten-rest/src/main/java/be/jasper/CertificatenRestApplication.java
@@ -0,0 +1,12 @@
+package be.jasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificatenRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificatenRestApplication.class, args);
+	}
+}
