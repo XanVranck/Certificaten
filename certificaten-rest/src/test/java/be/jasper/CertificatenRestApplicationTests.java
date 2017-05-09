84cc6cb7ba23fc4d3568a83c1d42b5ff772df3ab
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-05-09 21:05:14 +0200

    trying travis

--- certificaten-rest/src/test/java/be/jasper/CertificatenRestApplicationTests.java
+++ certificaten-rest/src/test/java/be/jasper/CertificatenRestApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CertificatenRestApplicationTests {
- 
+
 	@Test
 	public void contextLoads() {
 	}
