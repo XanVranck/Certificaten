65efba9a24dba9df5171bcf28a940713f1af3505
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-06-01 09:43:31 +0200

    trying te read PDF, but not working

--- certificaten-rest/src/main/java/be/jasper/domain/pdf/PDFReader.java
+++ certificaten-rest/src/main/java/be/jasper/domain/pdf/PDFReader.java
@@ -6,30 +6,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 @Configuration
 @EnableScheduling
 public class PDFReader {
     private String baseUrl = "C:\\wd\\pdf\\";
     private File folder = new File(baseUrl);
+    private BufferedWriter pdfToTxt;
+    private String klant = "";
 
-    @Scheduled(fixedDelay=5000)
+
+    @Scheduled(fixedDelay = 50000)
     public void pdfLezer() throws IOException {
         File[] listOfFiles = folder.listFiles();
         BufferedWriter bufferedWriter = txtFileVoorbereiden();
+        pdfToTxt = pdfToTxtVoorbeiden();
         int count = 0;
-        
+
         for (File file : listOfFiles) {
+            String text = "";
             if (file.isFile()) {
                 try {
-                    String text = getText(file);
+                    text = getText(file);
                     //TODO: SOUT verwijderen
                     System.out.println("Text in PDF: " + text);
                     schrijfWegNaarTxtFile(bufferedWriter, file.getName());
+                    schrijfWegNaarTxtFile(pdfToTxt, text);
                     ++count;
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -38,6 +41,13 @@ public class PDFReader {
         }
         schrijfWegNaarTxtFile(bufferedWriter, count + " PDF's in totaal!");
         bufferedWriter.close();
+    }
+
+    private BufferedWriter pdfToTxtVoorbeiden() throws IOException {
+        FileWriter fileWriter = new FileWriter(baseUrl + "pdfToTxt.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.flush();
+        return bufferedWriter;
     }
 
     private BufferedWriter txtFileVoorbereiden() throws IOException {
@@ -56,5 +66,23 @@ public class PDFReader {
     public String getText(File pdfFile) throws IOException {
         PDDocument doc = PDDocument.load(pdfFile);
         return new PDFTextStripper().getText(doc);
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void readTextFromTxt() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(baseUrl + "pdfToTxt.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+        int sizeOfTextFile = 91;
+        int klantLine = 1;
+        for (int line = 0; line <= sizeOfTextFile; line++) {
+            if (line == klantLine || line == 89) {
+                klant = bufferedReader.readLine();
+                System.out.println(klant);
+            }
+//            if (line == sizeOfTextFile-1 && !bufferedReader.readLine().isEmpty()) {
+//                sizeOfTextFile += 90;
+//                klantLine +=90;
+//            }
+        }
     }
 }
